0ce6b4b6ce08a31eea64f038ffcc96d4ec837bb9
Shri Krishna Sivakumar <shrikrishna0219@gmail.com>
2024-01-28 13:02:44 -0800

    I added two motors, one at port 1 and one at port 2, and created three methods in which the speeds of these motors are set. The last method uses two parameters.

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -5,25 +5,34 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import frc.robot.Constants.OperatorConstants;
 
 
 
 
 public class ArmSubsystem extends SubsystemBase {
 
-    private final CANSparkMax arm; 
+    private final CANSparkMax arm1; 
+    private final CANSparkMax arm2;
     private double speed; 
     public ArmSubsystem() {
-        this.arm = new CANSparkMax(Constants.ARM_MOTOR_1_PORT, MotorType.kBrushless);
-        this.arm = new CANSparkMax(Constants.ARM_MOTOR_2_PORT, MotorType.kBrushless);
+        this.arm1 = new CANSparkMax(OperatorConstants.ARM_MOTOR_1_PORT, MotorType.kBrushless);
+        this.arm2 = new CANSparkMax(OperatorConstants.ARM_MOTOR_2_PORT, MotorType.kBrushless);
 
     }
 
-    MotorSpeed = 0.5;
-    public void frc.robot.subsystems.setMotorSpeed(ARM_MOTOR_1_PORT) = (MotorSpeed)
-    public void setMotorSpeed(ARM_MOTOR_2_PORT) = Math.sin(MotorSpeed)/2
+    public void setMotor1Speed(double speed) {
+        this.arm1.set(speed);
+    }
 
+    public void setMotor2Speed(double speed) {
+        this.arm2.set(speed);
+    }
+
+    public void setMotorSpeeds(double speed1, double speed2) {
+        this.arm1.set(speed1);
+        this.arm2.set(speed2);
+    }
 
 
     
